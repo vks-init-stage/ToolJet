4f402227f1a4d114fb7d74b0f0f730aa5b56254a
Nevil Macwan <93511721+nevilm-lt@users.noreply.github.com>
2022-06-30 22:34:32 +0530

    updated escape

--- server/setup-postgres.js
+++ server/setup-postgres.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function lsExample() {
   try {
-    const { stdout, stderr } = await exec('apt install postgresql build-essential postgresql-contrib -y && /etc/init.d/postgresql start && su -c "psql -c \"ALTER USER postgres PASSWORD \'postgres\';\"" postgres && npm run db:create && npm run db:migrate');
+    const { stdout, stderr } = await exec('apt install postgresql build-essential postgresql-contrib -y && /etc/init.d/postgresql start && su -c "psql -c \\"ALTER USER postgres PASSWORD \'postgres\';\\"" postgres && npm run db:create && npm run db:migrate');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
   } catch (e) {
