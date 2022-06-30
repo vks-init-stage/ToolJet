1ab35fbbd270466ac04bc4d962f41274fc63729c
Nevil Macwan <93511721+nevilm-lt@users.noreply.github.com>
2022-06-30 22:47:49 +0530

    a

--- server/setup-postgres.js
+++ server/setup-postgres.js
@@ -2,15 +2,16 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function lsExample() {
-  try {
-    const { stdout, stderr } = await exec('chmod 777 /usr/local/bin/bundle && apt install postgresql build-essential postgresql-contrib -y && /etc/init.d/postgresql start && su -c "psql -c \\"ALTER USER postgres PASSWORD \'postgres\';\\"" postgres && npm run db:create && npm run db:migrate');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        const { stdout, stderr } = await exec('chmod 777 /usr/local/bin/bundle && apt install postgresql build-essential postgresql-contrib -y && /etc/init.d/postgresql start && su -c "psql -c \\"ALTER USER postgres PASSWORD \'postgres\';\\"" postgres && npm run db:create && npm run db:migrate');
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 module.exports = async () => {
     await lsExample()
 }
+
